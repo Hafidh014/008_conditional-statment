636ede2a04d231ee5b817ce52eebfaaabebb1124
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-03-10 12:19:40 +0700

    conditional statement

---
+++ p3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+float hitungRerata(int x, int y){
+    return (x + y) / 2;
+}
+
+string statusKelulusan(float a){
+    if(a >= 75){
+        return "Lulus";
+    }else{
+        return "Gagal";
+    }
+}
+
+int main(){
+    int nilai1,nilai2;
+    float rerata;
+    string status;
+
+
+    cout << "Masukkan nilai pertama: ";
+    cin >> nilai1;
+    cout << "Masukkan nilai kedua: ";
+    cin >> nilai2;
+    cout << "status kelulusan: " << statusKelulusan(hitungRerata(nilai1, nilai2)) << endl;
+}
